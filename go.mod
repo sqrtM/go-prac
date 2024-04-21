bda260ad12a992c12a8b751a10b88c1ee3ff2376
mason :) <masonapike@gmail.com>
2024-04-21 19:45:24 +0200

    db connection

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module main
+
+go 1.22
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
+	"os"
 )
 
 func main() {
-	fmt.Println("!... Hello World ...!")
+
+	connectToDb()
+
+}
+
+func connectToDb() {
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", "db", 5432, os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+
+	fmt.Println(psqlconn)
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			fmt.Printf("Database was not closed gracefully. %s\n", err)
+		}
+	}(db)
+
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
